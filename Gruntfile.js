bd6f3e9681209313cb01c8ec3b637f1caaa72f54
John Wu <webmaster@leapoahead.com>
2014-09-16 15:12:33 +0800

    auto sprite generation

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,14 +36,37 @@ module.exports = function(grunt) {
                     {expand: true, cwd: 'prepub', src: ['**'], dest: 'build/'}
                 ]
             }
+        },
+
+
+        sprite: {
+            icon: {
+                src: ['img/icon/*.png'],
+                destImg: 'prepub/img/icon/icon.png',
+                destCSS: 'less/bootstrap/icon.less',
+                padding: 2,
+                cssFormat: 'less',
+                imgPath: '../img/icon/icon.png'
+            },
+            icon_2x: {
+                src: ['img/icon-2x/*.png'],
+                destImg: 'prepub/img/icon/icon-2x.png',
+                destCSS: 'less/bootstrap/icon-2x.less',
+                padding: 2,
+                cssFormat: 'less',
+                imgPath: '../img/icon/icon-2x.png'
+            }
         }
     });
 
     require('load-grunt-tasks')(grunt);
 
+    // 预发布
+    grunt.registerTask('prepub:image', ['sprite']);
     grunt.registerTask('prepub:css', ['less:prepub']);
     grunt.registerTask('prepub', ['prepub:css']);
 
+    // 发布环境
     grunt.registerTask('build:pre', ['copy:build']);
     grunt.registerTask('build:css', ['cssmin:build']);
     grunt.registerTask('build', ['build:pre', 'build:css']);
